f037716fc8b2e78173fe441c4de6f6f2fbae00b2
snoberyman <yaman.sanobar@gmail.com>
2024-02-11 18:36:26 -0800

    navbar, theming, and hero text

---
+++ src/components/Topbar.js
@@ -0,0 +1,23 @@
+import Container from "react-bootstrap/Container";
+// import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./Topbar.css";
+
+function Topbar() {
+  return (
+    <>
+      <Navbar>
+        <Container>
+          <Navbar.Brand>Stackflex</Navbar.Brand>
+          {/* <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav> */}
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Topbar;
